d4164e3413edf7ab21ad02d85497dc8fd29abeed
Brian Burrous <brianon007@gmail.com>
2023-04-22 15:09:24 -0700

    added script

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = os.path.join('app', 'static/uploads')
+    AWS_ACCESS_KEY_ID='AKIASVLUVDCSMNV4YE7O'
+    AWS_SECRET_ACCESS_KEY='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'
+    S3_BUCKET='lahacks2023-ky-austin-riley-brian'
