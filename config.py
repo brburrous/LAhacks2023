57730d8887e75e34cbfba1e5a7966b044ed26cb7
Brian Burrous <brianon007@gmail.com>
2023-04-22 18:19:26 -0700

    working s3 upload

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     AWS_ACCESS_KEY_ID='AKIASVLUVDCSJ7TS36GP'
     AWS_SECRET_ACCESS_KEY='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'
     S3_BUCKET='lahacks2023-ky-austin-riley-brian'
+
