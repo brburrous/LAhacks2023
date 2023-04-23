dd3911d53fabe88f95b01bc9fd8c57f7eb38e180
Brian Burrous <brianon007@gmail.com>
2023-04-22 20:11:43 -0700

    foo

--- config.py
+++ config.py
@@ -4,5 +4,3 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = os.path.join('app', 'static/uploads')
     
-=======
->>>>>>> b06bffb (idk what i just did lol)
