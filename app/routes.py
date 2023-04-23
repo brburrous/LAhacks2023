129c9e2f477694bac4d3e3c00432158bc5b507c5
Brian Burrous <brianon007@gmail.com>
2023-04-22 19:53:11 -0700

    test

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def uploadFiles():
         # Upload file to database (defined uploaded folder in static path)
         print(app.config["UPLOAD_FOLDER"])
         print("foo")
-        os.chdir("app")
+        os.chdir("app/static")
         print(os.listdir())
         uploaded_img.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
         # Storing uploaded file path in flask session
