9a7870a8886c91f19fda6eba4d7ff4ae5b854d09
Brian Burrous <brianon007@gmail.com>
2023-04-22 19:24:46 -0700

    asdf

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = os.path.join('app', 'static/uploads')
+<<<<<<< HEAD
     
+=======
+>>>>>>> a59686a (asdf)
