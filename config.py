3849798d92e8c83c35fc0dbf8b7c400be06d394c
Brian Burrous <brianon007@gmail.com>
2023-04-22 18:19:26 -0700

    working s3 upload

--- config.py
+++ config.py
@@ -3,7 +3,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = os.path.join('app', 'static/uploads')
-<<<<<<< HEAD
     
-=======
->>>>>>> a59686a (asdf)
+
