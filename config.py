d4cf0a83727ca0d4a9713c163957d9f44e8c30a4
Brian Burrous <brianon007@gmail.com>
2023-04-22 19:22:49 -0700

    removed secret key

--- config.py
+++ config.py
@@ -3,6 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = os.path.join('app', 'static/uploads')
-    AWS_ACCESS_KEY_ID='AKIASVLUVDCSJ7TS36GP'
-    AWS_SECRET_ACCESS_KEY='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'
-    S3_BUCKET='lahacks2023-ky-austin-riley-brian'
+    
